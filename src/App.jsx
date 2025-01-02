f5a57f9c859467409bb167f7350bcf314fc9a9db
himesh kashyap <himeshkashyap347@gmail.com>
2025-01-03 03:39:35 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
 import { Button } from 'antd';
+import { useState } from 'react';
 function App() {
+const [counter,setCounter]=useState(15);
+
+
+  const addValue=()=>{
+    if(counter<20){
+     setCounter(counter+1)  
+    
+    }else{
+      return
+    }
+  }
+  const removeValue=()=>{
+    if(counter>0){
+      setCounter(counter-1)  
+ 
+    }else{
+      return;
+    }
+   }
 
   return (
     <>
-  <div className="App">
-    <Button type="">Button</Button>
-  </div>
+    <div className='flex align-center justify-center h-[100vh] flex-col  gap-2 text-center w-[300px] mx-auto'>
+    <h3>Counter value: {counter}</h3>
+
+    <button className='bg-blue-400'  onClick={addValue}>Add value{counter}</button>
+
+    <button className='bg-blue-400'   onClick={removeValue}>Remove value{counter}</button>
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    c
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
